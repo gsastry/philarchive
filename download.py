a0c69f1f529db5f03ae7bac510db7070d417940a
Girish Sastry <g.sastry@gmail.com>
2019-10-17 16:11:13 -0700

    Only get available papers

--- download.py
+++ download.py
@@ -1,6 +1,8 @@
 from oaipmh.client import Client
 from oaipmh.metadata import MetadataRegistry, oai_dc_reader
 
+import os
+
 def harvest(url):
     registry = MetadataRegistry()
     registry.registerReader('oai_dc', oai_dc_reader)
@@ -10,10 +12,19 @@ def harvest(url):
 
     identifiers = []
     for header in client.listIdentifiers(metadataPrefix='oai_dc'):
-        identifiers.append(header.identifier())
+        if (not(header.isDeleted())):
+            print(f"Appending {header.identifier()}")
+            identifiers.append(header.identifier())
+        else:
+            print(f"Skipping (DELETED) identifier {header.identifier()}")
     
-    with open('philarchive.txt', 'w') as f:
+    print(f"Total number of identifiers: {len(identifiers)}")
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'philarchive.txt')
+    with open(filename, 'w') as f:
+        print(f"Writing to {filename}")
         f.writelines('\n'.join(identifiers))
+
 
 if __name__ == "__main__":
     url = 'https://philarchive.org/oai.pl'
